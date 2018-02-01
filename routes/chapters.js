338cf166fc0f58a2d258ac124cf69e0898afad03
Priyank Upadhyay <priyank@platinos.in>
2018-02-02 03:23:31 +0530

    qwerty

--- routes/chapters.js
+++ routes/chapters.js
@@ -10,8 +10,8 @@ router.get('/', function(req, res, next) {
 	  		res.send(JSON.stringify({"status": 500, "error": error, "response": null})); 
 	  		//If there is error, we send the error in the error section with 500 status
 	  	} else {
-	  		res.charset = 'utf8';
-	  		res.setHeader('Content-Type', 'application/json');
+	  		//res.charset = 'utf8';
+	  		res.setHeader('Content-Type', 'application/json; charset=utf-8');
   			res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
   			//If there is no error, all is good and response is 200OK.
 	  	}
