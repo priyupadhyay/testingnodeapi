6ca04eb85a2bf09c529e70cf9376b16f0893ab4f
Priyank Upadhyay <priyank@platinos.in>
2018-02-03 02:00:50 +0530

    qwerty

--- routes/questions.js
+++ routes/questions.js
@@ -17,4 +17,18 @@ router.get('/', function(req, res, next) {
   	});
 });
 
+router.get('/:qid', function(req, res, next) {
+	connection.query('SELECT * FROM questions WHERE `status`= 1 AND id = '+req.qid, function (error, results, fields) {
+	  	if(error){
+	  		res.setHeader('Content-Type', 'application/json');
+	  		res.send(JSON.stringify({"status": 500, "error": error, "response": null})); 
+	  		//If there is error, we send the error in the error section with 500 status
+	  	} else {
+	  		res.setHeader('Content-Type', 'application/json');
+  			res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
+  			//If there is no error, all is good and response is 200OK.
+	  	}
+  	});
+});
+
 module.exports = router;
