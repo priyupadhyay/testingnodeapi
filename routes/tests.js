0525510b14be9c378df5c197d6828f3f0bb2ad2c
ishu-1234 <36048268+ishu-1234@users.noreply.github.com>
2018-02-04 22:33:29 +0530

    abc

--- routes/tests.js
+++ routes/tests.js
@@ -64,3 +64,20 @@ router.get('/:testId', function(req, res, next) {
 	  	}
   	});
 });
+
+
+router.get('/history/:studentId', function(req, res, next) {
+	connection.query('SELECT * FROM tests WHERE scheduled_time < NOW() AND studentId='+studentId, function (error, results, fields) {
+	  	if(error){
+	  		res.setHeader('Content-Type', 'application/json');
+	  		res.send(JSON.stringify({"status": 500, "error": error, "response": null})); 
+	  		//If there is error, we send the error in the error section with 500 status
+	  	} else {
+	  		//res.charset = 'utf8';
+	  		res.setHeader('Content-Type', 'application/json; charset=utf-8');
+  			res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
+  			//If there is no error, all is good and response is 200OK.
+	  	}
+  	});
+});
+module.exports = router;
