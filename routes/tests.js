b4fbadb84f29bef51bd0f60a5a00e433c086f738
ishu-1234 <36048268+ishu-1234@users.noreply.github.com>
2018-02-05 00:43:28 +0530

    ADDED post,put and delete functions in tests.js

--- routes/tests.js
+++ routes/tests.js
@@ -67,7 +67,7 @@ router.get('/:testId', function(req, res, next) {
 
 
 router.get('/history/:studentId', function(req, res, next) {
-	connection.query('SELECT * FROM tests WHERE tests.scheduled_time < NOW() AND tests.testId=user_tests.testId AND uesr_tests.studentId='+req.params.studentId, function (error, results, fields) {
+	connection.query('SELECT * FROM tests,user_tests WHERE tests.scheduled_time < NOW() AND tests.testId=user_tests.testId AND uesr_tests.studentId='+req.params.studentId, function (error, results, fields) {
 	  	if(error){
 	  		res.setHeader('Content-Type', 'application/json');
 	  		res.send(JSON.stringify({"status": 500, "error": error, "response": null})); 
@@ -80,4 +80,27 @@ router.get('/history/:studentId', function(req, res, next) {
 	  	}
   	});
 });
+
+router.post('/',function(req,res){
+  //var testId=req.body.testId;
+  //var scheduled_time=req.body.scheduled_time;
+  connection.query('INSERT INTO tests VALUES (?)',testId, function (error, results, fields) {});
+  res.end("yes");
+});
+
+
+router.put('/:testId',function(req,res){
+	//var testId=req.params.testId;
+	//var scheduled_time=req.body.scheduled_time;
+	connection.query('UPDATE tests SET scheduled_time =? WHERE testId = ? ',scheduled_time,testId, function (error, results, fields) {});
+	res.end("yes");
+  });
+
+
+router.delete('/:testId',function(req,res){
+	//var testId=req.params.testId;
+	//var scheduled_time=req.body.scheduled_time;
+	connection.query('DELETE FROM tests,testdetails WHERE testId = ? ',testId, function (error, results, fields) {});
+	res.end("Record Deleted");
+  });
 module.exports = router;
