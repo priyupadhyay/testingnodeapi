764a44ad5c03b3b1488510426b39b2c5eecf8f36
Priyank Upadhyay <priyank@platinos.in>
2018-02-10 01:20:18 +0530

    qwerty

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-
-
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -8,9 +6,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mysql= require('mysql');
 var http = require('http');
-
-
-
 var index = require('./routes/index');
 var users = require('./routes/users');
 var ui = require('./routes/ui');
@@ -23,7 +18,6 @@ var questionpaper = require('./routes/questionpaper');
 var tests = require('./routes/tests');
 var app = express();
 
-//app.use(require('connect').bodyParser());
 // view engine setup
 //app.set('views', path.join(__dirname, 'views'));
 
@@ -32,10 +26,8 @@ var app = express();
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(function(req, res, next) {
