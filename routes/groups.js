8c3c7f1b126ce0aea9d1236922fa76a35b562bf4
ishu-1234 <36048268+ishu-1234@users.noreply.github.com>
2018-02-10 03:32:11 +0530

    xcvbnm,

--- routes/groups.js
+++ routes/groups.js
@@ -37,7 +37,7 @@ router.get('/:id', function(req, res, next) {
 
 
 router.get('/:id/users', function(req, res, next) {
-	connection.query('SELECT * FROM groups t1, group_users t2, users t3 USING t1,t2,t3 WHERE t1.id =  t2.group_id AND t2.student_id = t3.user_id AND t1.id='+req.params.id, function (error, results, fields) {
+	connection.query('SELECT * FROM users WHERE user_id IN (SELECT student_id FROM group_users WHERE group_id = ?)',[req.params.id], function (error, results, fields) {
 	  	if(error){
 	  		res.setHeader('Content-Type', 'application/json');
 	  		res.send(JSON.stringify({"status": 500, "error": error, "response": null})); 
@@ -87,7 +87,7 @@ router.post('/',function(req,res){
 			
 			//res.charset = 'utf8';
 			res.setHeader('Content-Type', 'application/json; charset=utf-8');
-			res.send(JSON.stringify({"status": 200, "error": null, "response": {msg: "Record Inserted "+results.insertId}}));
+			res.send(JSON.stringify({"status": 200, "error": null, "response": {msg: "Record Updated "}}));
 			//If there is no error, all is good and response is 200OK.
 		}
   });
