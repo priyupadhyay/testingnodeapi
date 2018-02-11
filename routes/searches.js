eb00e0b5390304532bf57274f175770663f364c3
ishu-1234 <36048268+ishu-1234@users.noreply.github.com>
2018-02-11 20:28:38 +0530

    fhjkhk.

--- routes/searches.js
+++ routes/searches.js
@@ -18,10 +18,26 @@ router.get('/', function(req, res, next) {
   	});
 });
 
+//GET treanding searches
+router.get('/treanding', function(req, res, next) {
+	connection.query('SELECT * FROM questions WHERE id IN (SELECT DISTINCT question_id FROM searches ORDER BY time LIMIT 10)', function (error, results, fields) {
+	  	if(error){
+	  		res.setHeader('Content-Type', 'application/json');
+	  		res.send(JSON.stringify({"status": 500, "error": error, "response": null})); 
+	  		//If there is error, we send the error in the error section with 500 status
+	  	} else {
+	  		//res.charset = 'utf8';
+	  		res.setHeader('Content-Type', 'application/json; charset=utf-8');
+  			res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
+  			//If there is no error, all is good and response is 200OK.
+	  	}
+  	});
+});
+
 
 //GET searches using user id
 router.get('/:uid', function(req, res, next) {
-	connection.query('SELECT * FROM questions WHERE id IN (SELECT DISTINCT question_id FROM searches WHERE user_id = ? ORDER BY time)  LIMIT 10',[req.params.uid], function (error, results, fields) {
+	connection.query('SELECT * FROM questions WHERE id IN (SELECT DISTINCT question_id FROM searches WHERE user_id = ? ORDER BY time)',[req.params.uid], function (error, results, fields) {
 	  	if(error){
 	  		res.setHeader('Content-Type', 'application/json');
 	  		res.send(JSON.stringify({"status": 500, "error": error, "response": null})); 
