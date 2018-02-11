ed63df946eff2626e8fdb3da403b8e11a3d15be2
ishu-1234 <36048268+ishu-1234@users.noreply.github.com>
2018-02-11 20:13:30 +0530

    zxcvbnm

--- routes/searches.js
+++ routes/searches.js
@@ -20,7 +20,7 @@ router.get('/', function(req, res, next) {
 
 
 //GET searches using user id
-router.get('/my/:uid', function(req, res, next) {
+router.get('/:uid', function(req, res, next) {
 	connection.query('SELECT * FROM questions WHERE id IN (SELECT DISTINCT question_id FROM searches WHERE user_id = ? ORDER BY time LIMIT 10)',[req.params.uid],[req.params.uid], function (error, results, fields) {
 	  	if(error){
 	  		res.setHeader('Content-Type', 'application/json');
