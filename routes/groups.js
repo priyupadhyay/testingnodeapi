5066260e3540d7245497be99db486c38786dd6b5
ishu-1234 <36048268+ishu-1234@users.noreply.github.com>
2018-02-11 12:27:18 +0530

    sdfghjkljhgf

--- routes/groups.js
+++ routes/groups.js
@@ -19,7 +19,24 @@ router.get('/', function(req, res, next) {
 });
 
 
+//GET groups using user id
+router.get('/my/:uid', function(req, res, next) {
+	connection.query('SELECT * FROM groups WHERE status=1 AND id IN(SELECT group_id from group_users WHERE status=1 AND student_id=?)',[req.params.uid], function (error, results, fields) {
+	  	if(error){
+	  		res.setHeader('Content-Type', 'application/json');
+	  		res.send(JSON.stringify({"status": 500, "error": error, "response": null})); 
+	  		//If there is error, we send the error in the error section with 500 status
+	  	} else {
+	  		//res.charset = 'utf8';
+	  		res.setHeader('Content-Type', 'application/json; charset=utf-8');
+  			res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
+  			//If there is no error, all is good and response is 200OK.
+	  	}
+  	});
+});
 
+
+//GET groups using group id
 router.get('/:id', function(req, res, next) {
 	connection.query('SELECT * FROM groups WHERE status=1 AND id='+req.params.id, function (error, results, fields) {
 	  	if(error){
