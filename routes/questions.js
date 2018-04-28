4a73b5aee6c02069f751706d113b4508a3614e3a
Priyank Upadhyay <priyank@platinos.in>
2018-04-28 13:45:02 +0530

    sdv

--- routes/questions.js
+++ routes/questions.js
@@ -16,22 +16,17 @@ router.get('/', function(req, res, next) {
 	  	}
   	});
 });
-/* GET chapters listing. */
-router.get('/all', function (req, res, next) {
-	connection.query('SELECT * FROM questions WHERE `status`= 1', function (error, results, fields) {
-		if (error) {
-			res.setHeader('Content-Type', 'application/json');
-			res.send(JSON.stringify({ "status": 500, "error": error, "response": null }));
-			//If there is error, we send the error in the error section with 500 status
-		} else {
-			res.setHeader('Content-Type', 'application/json');
-			res.send(JSON.stringify({ "status": 200, "error": null, "response": results }));
-			//If there is no error, all is good and response is 200OK.
-		}
-	});
-});
+
+
 router.get('/:qid', function(req, res, next) {
-	var sql = 'SELECT * FROM questions WHERE id = '+req.params.qid;
+	var sql = '';
+	if(req.params.qid === 'all'){
+		/* GET all questions listing. */
+		sql = 'SELECT * FROM questions WHERE `status`= 1';
+		}	
+		/* GET question by id listing. */
+		else sql = 'SELECT * FROM questions WHERE id = '+req.params.qid;
+
 	connection.query( sql, function (error, results, fields) {
 	  	if(error){
 	  		res.setHeader('Content-Type', 'application/json');
